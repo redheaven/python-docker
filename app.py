b9f352cb92c60f713a74ae3b67a1ecc7fd4ec11a
redheaven <73341052+redheaven@users.noreply.github.com>
2021-06-30 03:21:58 +0300

    Update app.py

--- app.py
+++ app.py
@@ -18,28 +18,6 @@ def coffee():
   return "It's time to coffee"
 
 
-@app.route("/shutdown", methods = ['POST'])
-def shutdown_server():
-	print("Shutdown context hit with POST!")
-	if request.form.get('username') and request.form.get('password'):
-		print('Got username: {} and password: {}'.format(request.form.get('username'),request.form.get('password')))
-		if request.form.get('username') != user:
-			return 'The username or password is incorrect!'
-
-		if request.form.get('password') != pwd:
-			return 'The username or password is incorrect!'
-
-		print("It seems to be valid, the server is shutting down!")
-		shutdown = request.environ.get('werkzeug.server.shutdown')
-		if shutdown is None:
-			raise RuntimeError('The function is unavailable!')
-		else:
-			shutdown()
-			return "THe server is shutting down!"
-
-	else:
-		return 'You need authorization to shut the server down!'
-
 @app.route('/initdb')
 def db_init():
   mydb = mysql.connector.connect(
