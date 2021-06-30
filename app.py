7be3c1698ec5f691d519888ec32e287bc5d635dd
redheaven <73341052+redheaven@users.noreply.github.com>
2021-06-30 03:20:44 +0300

    Update app.py

--- app.py
+++ app.py
@@ -20,8 +20,16 @@ def coffee():
 
 @app.route("/shutdown", methods = ['POST'])
 def shutdown_server():
-	  print("Shutdown context hit with POST!")
-	
+	print("Shutdown context hit with POST!")
+	if request.form.get('username') and request.form.get('password'):
+		print('Got username: {} and password: {}'.format(request.form.get('username'),request.form.get('password')))
+		if request.form.get('username') != user:
+			return 'The username or password is incorrect!'
+
+		if request.form.get('password') != pwd:
+			return 'The username or password is incorrect!'
+
+		print("It seems to be valid, the server is shutting down!")
 		shutdown = request.environ.get('werkzeug.server.shutdown')
 		if shutdown is None:
 			raise RuntimeError('The function is unavailable!')
@@ -29,6 +37,8 @@ def shutdown_server():
 			shutdown()
 			return "THe server is shutting down!"
 
+	else:
+		return 'You need authorization to shut the server down!'
 
 @app.route('/initdb')
 def db_init():
@@ -59,4 +69,3 @@ def db_init():
 
 if __name__ == "__main__":
   app.run(host ='0.0.0.0', threaded=True)
-shutdown
