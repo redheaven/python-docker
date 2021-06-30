77b163adf176b7a7d9412915b04fc81ad51d5166
redheaven <73341052+redheaven@users.noreply.github.com>
2021-06-30 03:13:10 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
+from logging import shutdown
 import mysql.connector
 from flask import Flask
 import atexit
 
 
 app = Flask(__name__)
-
-
 
 
 @app.route('/')
@@ -17,6 +16,29 @@ def coffee():
     database="inventory"
   )
   return "It's time to coffee"
+
+
+@app.route("/shutdown", methods = ['POST'])
+def shutdown_server():
+	print("Shutdown context hit with POST!")
+	if request.form.get('username') and request.form.get('password'):
+		print('Got username: {} and password: {}'.format(request.form.get('username'),request.form.get('password')))
+		if request.form.get('username') != user:
+			return 'The username or password is incorrect!'
+
+		if request.form.get('password') != pwd:
+			return 'The username or password is incorrect!'
+
+		print("It seems to be valid, the server is shutting down!")
+		shutdown = request.environ.get('werkzeug.server.shutdown')
+		if shutdown is None:
+			raise RuntimeError('The function is unavailable!')
+		else:
+			shutdown()
+			return "THe server is shutting down!"
+
+	else:
+		return 'You need authorization to shut the server down!'
 
 @app.route('/initdb')
 def db_init():
@@ -47,3 +69,4 @@ def db_init():
 
 if __name__ == "__main__":
   app.run(host ='0.0.0.0', threaded=True)
+shutdown
