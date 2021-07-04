2cdd9e0ae8019cc9a7a48bf9c65ed5e61b4fbc4a
redheaven <73341052+redheaven@users.noreply.github.com>
2021-07-04 23:28:51 +0300

    Update app.py

--- app.py
+++ app.py
@@ -12,24 +12,8 @@ def index():
 @app.route("/shutdown", methods = ['GET', 'POST'])
 def shutdown_server():
 	print("Shutdown context hit with POST!")
-	if request.form.get('username') and request.form.get('password'):
-		print('Got username: {} and password: {}'.format(request.form.get('username'),request.form.get('password')))
-		if request.form.get('username') != user:
-			return 'The username or password is incorrect!'
-
-		if request.form.get('password') != pwd:
-			return 'The username or password is incorrect!'
-
-		print("It seems to be valid, the server is shutting down!")
-		shutdown = request.environ.get('werkzeug.server.shutdown')
-		if shutdown is None:
-			raise RuntimeError('The function is unavailable!')
-		else:
-			shutdown()
-			return "THe server is shutting down!"
-
-	else:
-		return 'You need authorization to shut the server down!'
+	shutdown()
+	return "THe server is shutting down!"
 
 if __name__ == '__main__':
 	app.run(port = 8081, host = '0.0.0.0', debug = True)
