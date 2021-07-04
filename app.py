e94f54d8e5374c4937a15160fb9b61e7cfe0c123
redheaven <73341052+redheaven@users.noreply.github.com>
2021-07-05 00:13:32 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,48 @@
-from flask import Flask, request
+import mysql.connector
+from flask import Flask
 
-user = ''
-pwd = ''
 
 app = Flask(__name__)
 
-@app.route("/", methods=['GET','POST'])
-def index():
-	return "Welcome world!"
 
-@app.route("/shutdown", methods = ['GET', 'POST'])
-def shutdown_server():
-	print("Shutdown context hit with POST!")
-	shutdown()
+@app.route('/')
+def coffee():
+  mydb = mysql.connector.connect(
+    host="db",
+    user="root",
+    password="p@ssw0rd1",
+    database="inventory"
+  )
+  return "It's time to coffee"
 
-if __name__ == '__main__':
-	app.run(port = 8081, host = '0.0.0.0', debug = True)
+
+@app.route('/initdb')
+def db_init():
+  mydb = mysql.connector.connect(
+    host="db",
+    user="root",
+    password="p@ssw0rd1"
+  )
+  cursor = mydb.cursor()
+
+  cursor.execute("DROP DATABASE IF EXISTS inventory")
+  cursor.execute("CREATE DATABASE inventory")
+  cursor.close()
+
+  mydb = mysql.connector.connect(
+    host="db",
+    user="root",
+    password="p@ssw0rd1",
+    database="inventory"
+  )
+  cursor = mydb.cursor()
+
+  cursor.execute("DROP TABLE IF EXISTS widgets")
+  cursor.execute("CREATE TABLE widgets (name VARCHAR(255), description VARCHAR(255))")
+  cursor.close()
+  
+
+  return 'Here is nothing to do'
+
+if __name__ == "__main__":
+  app.run(host ='0.0.0.0')
